35cca969f91a8107f0715bc235f9493164615b73
Victor Lesk <victorlesk@hotmail.com>
2017-09-20 16:24:51 +0100

    Initial Commit

---
+++ MatImms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatImms
+//
+//  Created by Victor Lesk on 20/09/2017.
+//  Copyright © 2017 Digital Stitch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
